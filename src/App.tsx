fdebf9b7f73468d8874f655e3431295a2f0379c1
Glovindan <glovindan@demasscult.fun>
2021-02-05 00:04:13 +0400

    i did a shit

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://demasscult.ru"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn My Cock
         </a>
       </header>
     </div>
