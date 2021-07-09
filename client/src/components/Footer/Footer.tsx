fd4059a88c4e312519197dcc376602549a4c15bc
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-07-09 12:51:23 +0300

    some visual fix

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import styles from './Footer.module.scss'
 const Footer = ():React.ReactElement => {
   return (
     <footer className={styles.container}>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et rerum necessitatibus exercitationem optio, cupiditate aspernatur cumque officiis suscipit modi unde odit, asperiores est amet, omnis corporis quasi. At, repellat odit?
+      2020 - 2021
     </footer>
   )
 };
--- client/src/pages/homePage/components/userProfile/UserProfile.tsx
+++ client/src/pages/homePage/components/userProfile/UserProfile.tsx
@@ -1,34 +1,32 @@
 import React, { useContext } from 'react';
-import Button from '../../../../components/Button';
-import AuthContext from '../../../../context/AuthContext';
 
 import styles from './UserProfile.module.scss';
 
 const UserProfile = (): React.ReactElement => {
-  const { logout } = useContext(AuthContext);
-
+  
   return (
-    <div className={styles.profile}>
-      <div className={styles.wrapperAvatar}>
-        <img className={styles.avatar} alt={'avatar'} />
-      </div>
-      <div className={styles.info}>
-        <h2 className={styles.username}>USERNAME</h2>
-        <div className={styles.stats}>
-          <ul className={styles.list}>
-            <li className={styles.element}>
-              <span>Кол-во слов в словаре:</span>
-            </li>
-            <li className={styles.element}>
-              <span>Кол-во изученных слов:</span>
-            </li>
-            <li className={styles.element}>
-              <span>Кол-во слов для изучения:</span>
-            </li>
-          </ul>
+    <div className={styles.wrapper}>
+      <div className={styles.profile}>
+        <div className={styles.info}>
+          <h2 className={styles.username}>USERNAME</h2>
+          <div className={styles.stats}>
+            <ul className={styles.list}>
+              <li className={styles.element}>
+                <span>Кол-во слов в словаре: </span>
+                <span>12566</span>
+              </li>
+              <li className={styles.element}>
+                <span>Кол-во изученных слов: </span>
+                <span>7667</span>
+              </li>
+              <li className={styles.element}>
+                <span>Кол-во слов для изучения:</span>
+                <span>4899</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <Button classNameUpdate={styles.logout} onClick={logout} text={'Выйти из системы (logout)'} />
     </div>
   );
 }
