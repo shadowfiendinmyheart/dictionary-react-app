1083366750f0a78ddffc5858d31a0b2f9d571662
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-07-10 12:00:59 +0300

    changes for loader

--- client/src/hooks/http.hook.ts
+++ client/src/hooks/http.hook.ts
@@ -10,33 +10,33 @@ export const useHttp = () => {
       body: object | string | null = null,
       headers: any = {}
     ) => {
-    try {
-      if (body) {
-        body = JSON.stringify(body);
-        headers['Content-Type'] = 'application/json';
-      }
+      try {
+        if (body) {
+          body = JSON.stringify(body);
+          headers['Content-Type'] = 'application/json';
+        }
 
-      setLoading(true);
-      const response: Response = await fetch(url, {method, body, headers});
-      const data: any = await response.json();
-      
-      if (!response.ok) {
-        throw new Error(data.message || 'Error . . .')
+        setLoading(true);
+        const response: Response = await fetch(url, {method, body, headers});
+        const data: any = await response.json();
+        
+        if (!response.ok) {
+          throw new Error(data.message || 'Error . . .')
+        }
+        
+        setLoading(false);
+        if (data.message) {
+          setAnswer(data.message);
+        }
+        return data;
+      } catch (e) {
+        setLoading(false);
+        if (e.message) {
+          setAnswer(e.message);
+        }
+        throw e;
       }
-      
-      setLoading(false);
-      if (data.message) {
-        setAnswer(data.message);
-      }
-      return data;
-    } catch (e) {
-      setLoading(false);
-      if (e.message) {
-        setAnswer(e.message);
-      }
-      throw e;
-    }
-  }, [])
+    }, [])
 
   const clearAnswer = () => setAnswer(null);
 
